fa885ad34b558a3a9ac0df76f9c12ce9edee4ad3
Akshat Naruka <79619620+AkshatNaruka@users.noreply.github.com>
2024-06-25 19:26:01 +0530

    quotes corrected

--- clockex/src/components/QuoteBox.js
+++ clockex/src/components/QuoteBox.js
@@ -5,24 +5,27 @@ function QuoteBox() {
 
   useEffect(() => {
     const fetchQuote = async () => {
-      const response = await fetch('https://api.quotable.io/quotes/random?tags=Wisdom');
-      const data = await response.json();
-      setQuote(data[0]);
+      try {
+        const response = await fetch('https://api.quotable.io/quotes/random?tags=Wisdom');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setQuote(data[0]);
+      } catch (error) {
+        const localQuotes = [
+          { content: "The only way to do great work is to love what you do.", author: "Steve Jobs" },
+          { content: "Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful.", author: "Albert Schweitzer" },
+          { content: "The best way to predict the future is to create it.", author: "Peter Drucker" },
+          { content: "The only limit to our realization of tomorrow will be our doubts of today.", author: "Franklin D. Roosevelt" },
+          { content: "The future belongs to those who believe in the beauty of their dreams.", author: "Eleanor Roosevelt" }
+        ];
+        const randomIndex = Math.floor(Math.random() * localQuotes.length);
+        setQuote(localQuotes[randomIndex]);
+      }
     };
 
-    if (navigator.onLine) {
-      fetchQuote();
-    } else {
-      const localQuotes = [
-        { content: "The only way to do great work is to love what you do.", author: "Steve Jobs" },
-        { content: "Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful.", author: "Albert Schweitzer" },
-        { content: "The best way to predict the future is to create it.", author: "Peter Drucker" },
-        { content: "The only limit to our realization of tomorrow will be our doubts of today.", author: "Franklin D. Roosevelt" },
-        { content: "The future belongs to those who believe in the beauty of their dreams.", author: "Eleanor Roosevelt" }
-      ];
-      const randomIndex = Math.floor(Math.random() * localQuotes.length);
-      setQuote(localQuotes[randomIndex]);
-    }
+    fetchQuote();
   }, []);
 
   return (
